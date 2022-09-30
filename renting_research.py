396d6613b948d9826b4b64ecfab39da5b6546395
Indu <70434933+indukvn@users.noreply.github.com>
2022-09-30 16:02:59 +0530

    Add files via upload

---
+++ renting_research.py
@@ -0,0 +1,47 @@
+import time
+
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+google_form = "your google form url"
+zillo_url = "https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
+response = requests.get(url=zillo_url,  headers={"Accept-Language": "your accept language", "User-Agent": "your data"})
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+links = soup.find_all(name="div", class_=".list-card-top a")
+
+all_links = []
+for link in links:
+    href = link["href"]
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address = soup.find_all(name="div", class_=".list-card-info address")
+addresses = [address.get_text().split("|")[-1] for address in all_address]
+
+all_price = soup.findAll(name="div", class_=".list-card-details li")
+prices = [price.get_text().split("+")[0] for price in all_price]
+
+chrome_driver_path = "your chrome driver path"
+
+driver = Chrome(chrome_driver_path)
+
+for n in range(len(all_links)):
+    driver.get(google_form)
+
+    time.sleep(2)
+
+    address = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_tag = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_btn = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+
+    address.send_keys(addresses[n])
+    price.send_keys(prices[n])
+    link_tag.send_keys(all_links[n])
+    submit_btn.click()
